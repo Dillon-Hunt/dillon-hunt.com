da282b6c4832f2f3de055d52d06dd7c214e9a2e7
Dillon <dillonhunt2005@gmail.com>
2021-12-07 00:10:54 +1000

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="About">
             <h1>About Me</h1>
-            <h2>Here Is A Small Overview Of Me!</h2>
+            <h2>Here Is A Small Overview Of Me</h2>
             <p>
                 My name is Dillon Hunt, I am 15 years old and I love programming. I live on the Sunshine Coast, Australia and I am currently in grade 10.
                 I like to create small websites in my spare time to improve my skills. When creating a website I usually use vanilla HTML, CSS and JavaScript.
