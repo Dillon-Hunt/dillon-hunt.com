4a15b10e8ec09529621fc34f5cf454e9808be332
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-03-21 18:53:53 +1000

    Fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
             <h1>About Me</h1>
             <h2>Here Is A Small Overview Of Me</h2>
             <p>
-                My name is Dillon Hunt, I am 15 years old and I love programming. I live on the Sunshine Coast, Australia and I am currently in grade 11.
+                My name is Dillon Hunt, I am 16 years old and I love programming. I live on the Sunshine Coast, Australia and I am currently in grade 11.
                 I like to create small websites in my spare time to improve my skills. 
                 When creating a website I usually use vanilla HTML, CSS and JavaScript.
                 I am a frontend programmer but I am trying to build my experience in full stack development. I enjoy collaborating with my friends to create projects that we couldn't create individually. 
